6f9f487a7a49d0835729e032759ff56aa87c1018
Nguyen Huu Nghia <nghiahtnb@gmail.com>
2020-11-04 13:39:26 +0700

    Updates

---
+++ precache-manifest.158b3d4ebe25b8ef5c173cf70d2b1b67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b8beb461a6fc1d46fc14f65284897c6",
+    "url": "/caro/index.html"
+  },
+  {
+    "revision": "0d4935ed3f6876d53da0",
+    "url": "/caro/static/css/main.8e84c22b.chunk.css"
+  },
+  {
+    "revision": "4cfc160bb3d13546e2db",
+    "url": "/caro/static/js/2.c5010d1a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/caro/static/js/2.c5010d1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d4935ed3f6876d53da0",
+    "url": "/caro/static/js/main.64ca2edb.chunk.js"
+  },
+  {
+    "revision": "c2f7e251ebaa760233eb",
+    "url": "/caro/static/js/runtime-main.b84f2f81.js"
+  }
+]);
